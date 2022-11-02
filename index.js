da2dd589f416960b651f7e81331ecbe6eef1b26d
Devil5614737 <kaushiksheel9@gmail.com>
2022-11-02 13:26:08 +0530

    added toast notification

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
 const btn=document.querySelector('.btn');
 
 
+function Toast(){
+    Toastify({
+        text: "Copied to Clipboard",
+        className: "info",
+        garavity:'center',
+      }).showToast();
+}
+
+function CopyDomainToClipboard(){
+    const domain=window.location.hostname;
+    navigator.clipboard.writeText(domain);
+   Toast()
+}
+
+
 btn.addEventListener('click',()=>{
-console.log(window.location.host)
+    CopyDomainToClipboard()
 })
+
+
